6f67107e86ad46bad6e6e421199a86e72aa6c6c9
Ragini kumari <88733937+Raginikumari15@users.noreply.github.com>
2023-02-20 08:16:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let a =null;
+let b=678;
+let c= true;
+let d =BigInt(123)
+let e= "ragini"
+let f= undefined;
+//let g =symbol("ABC");
+console.log(a,b,c,d,e,f)
+console.log(typeof e)
+//nn bb  ss u
+//null, number, bool, byte, string symbol, undefined.
+
+//object
+const item =
+{
+ "harry": true,
+  "shubham": false,
+  "LOvish": false,
+  "Rohan": undefined
+  
+}
+console.log(item["shubham"])
